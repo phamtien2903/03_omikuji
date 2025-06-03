846ea8f39b85c5370bfd027c354aff595d432a33
phamtien2903 <49803027+phamtien2903@users.noreply.github.com>
2025-06-03 13:46:47 +0900

    sw

--- js/omikuji.js
+++ js/omikuji.js
@@ -2,3 +2,27 @@ window.addEventListener( "DOMContentLoaded" , function(){
     const popMessager ="いらっしゃい！おみくじ引いてって！";
     this.window.alert(popMessager);
 })
+const btn1 = document.getElementById("btn1");     // [A]
+btn1.addEventListener("click",                    // [B]
+  function() {                                    // [C]
+    let n = Math.floor(Math.random() * 3);        // [D]
+
+    switch (n) {
+      case 0:
+        btn1.textContent = "Very Happy!";         // [E]
+        btn1.style.color = "#ff0000";             // [F]
+        break;
+
+      case 1:
+        btn1.textContent = "Happy!";
+        btn1.style.color = "#0000ff";
+        break;
+
+      case 2:
+        btn1.textContent = "UnHappy...";
+        btn1.style.color = "#261e1c";
+        break;
+    }
+  },
+  false
+);
